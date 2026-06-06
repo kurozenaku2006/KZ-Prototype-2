35981f7fb7fb9250aa42fb2369f802778c33ba9f
kurozenaku2006 <vedantbhalge2006@gmail.com>
2026-06-06 23:43:11 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,35 @@
+// ========================================
+// KUROZENAKU FIREBASE CONFIG
+// ========================================
 
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyByLURDPjEbLepc3Wz3O1MAYYLXSeu04Zg",
+  authDomain: "kurozenaku-gundam-store.firebaseapp.com",
+  projectId: "kurozenaku-gundam-store"
+};
+
+// Prevent duplicate initialization
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Firebase Services
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// Google Provider
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
+// Global Helper
+window.kzFirebase = {
+  db,
+  auth,
+  googleProvider
+};
+
+console.log("KUROZENAKU Firebase Loaded");
